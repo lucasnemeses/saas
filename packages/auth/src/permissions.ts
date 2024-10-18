eeaa158f28fe7377ba232a2f46a64d46bd65b2af
Lucas Meneses <lucasnemeses01@gmail.com>
2024-10-17 21:35:23 -0300

    permissions and roles

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,14 @@
+import { AbilityBuilder } from "@casl/ability"
+import { AppAbility } from "."
+
+type DefinePermissions = (user: unknown, builder: AbilityBuilder<AppAbility>) => void;
+export type Roles = 'admin' | 'member';
+
+export const permissions: Record<Roles, DefinePermissions> = {
+  admin(_, { can }) {
+    can('manage', 'all')
+  },
+  member(_, { can }) {
+    can('invite', 'User')
+  }
+}
